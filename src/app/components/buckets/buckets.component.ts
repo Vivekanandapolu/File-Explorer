f8c3df13eaf404b8824c745706ee7e95ec0a4389
vivkananda Reddy <vivekanandapolu123@gmail.com>
2023-10-13 12:58:04 +0530

    port chamged and error is successs  now

--- src/app/components/buckets/buckets.component.ts
+++ src/app/components/buckets/buckets.component.ts
@@ -7,133 +7,132 @@ import { TokenService } from 'src/app/token.service';
 @Component({
   selector: 'app-buckets',
   templateUrl: './buckets.component.html',
-  styleUrls: ['./buckets.component.scss']
+  styleUrls: ['./buckets.component.scss'],
 })
 export class BucketsComponent implements OnInit {
-  buckets: any = []
-  singleBucketsData: any = []
-  bucketsView = true
-  allBuckets: any = []
-  dataToSend: any[] = []
-  Loader: Boolean = false
-  BackIndexVal: any = 0
-  FrontIndexVal: any = 0
-  queryParams: any = {}
-  bucketNameGlobal: any
-  FileTypes: any = []
-  TabName: any = ''
-  clickedFolder: any = []
+  buckets: any = [];
+  singleBucketsData: any = [];
+  bucketsView = true;
+  allBuckets: any = [];
+  dataToSend: any[] = [];
+  Loader: Boolean = false;
+  BackIndexVal: any = 0;
+  FrontIndexVal: any = 0;
+  queryParams: any = {};
+  bucketNameGlobal: any;
+  FileTypes: any = [];
+  TabName: any = '';
+  clickedFolder: any = [];
 
-  extensionsArray: any = [".docx", ".pdf", ".xlsx", ".csv", ".pptx", ".jpg"]
-  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute, private tokenservice: TokenService) {
-  }
-  viewtype: any
-  dataOfFolder: any = []
+  extensionsArray: any = ['.docx', '.pdf', '.xlsx', '.csv', '.pptx', '.jpg'];
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private route: ActivatedRoute,
+    private tokenservice: TokenService
+  ) {}
+  viewtype: any;
+  dataOfFolder: any = [];
 
-
-  createdArrow: Boolean = true
-  sizeArrow: Boolean = true
-  nameArrow: Boolean = true
+  createdArrow: Boolean = true;
+  sizeArrow: Boolean = true;
+  nameArrow: Boolean = true;
 
   ngOnInit(): void {
-    this.tokenservice.validateTokenAndRefresh()
-    localStorage.setItem('tabname', 'Buckets')
-    this.viewtype = localStorage.getItem('view') || 'grid'
+    this.tokenservice.validateTokenAndRefresh();
+    localStorage.setItem('tabname', 'Buckets');
+    this.viewtype = localStorage.getItem('view') || 'grid';
 
-    this.bucketsView = false
+    this.bucketsView = false;
     this.route.queryParamMap.subscribe((params: any) => {
-      if (!params.has("mainbucket")) {
-        this.getBuckets()
-        this.bucketsView = true
-        this.singleBucketsData = []
-        this.TabName = params.get('name')
-        this.dataToSend = []
-      }
-      else {
+      if (!params.has('mainbucket')) {
+        this.getBuckets();
+        this.bucketsView = true;
+        this.singleBucketsData = [];
+        this.TabName = params.get('name');
+        this.dataToSend = [];
+      } else {
         this.getBucketsData(params.get('mainbucket')).then((res: any) => {
-          if (params.has("path")) {
-            this.getBucketsDataByPath(params.get('path'), params.get("mainbucket"))
+          if (params.has('path')) {
+            this.getBucketsDataByPath(
+              params.get('path'),
+              params.get('mainbucket')
+            );
           }
-        })
+        });
         if (params?.params?.data) {
-          this.dataToSend = JSON.parse(params?.params?.data)
+          this.dataToSend = JSON.parse(params?.params?.data);
         }
       }
-
     });
   }
-
 
   //Get Buckets
 
   getBuckets() {
-
-    this.allBuckets = []
+    this.allBuckets = [];
     this.http.get(apiurls.buckets).subscribe((res: any) => {
-      this.buckets = [res]
+      this.buckets = [res];
       for (let bucket in this.buckets) {
-        this.allBuckets.push(...this.buckets[bucket].buckets)
+        this.allBuckets.push(...this.buckets[bucket].buckets);
       }
 
       const queryParams = {
         backIndex: 0,
         frontIndex: 0,
         tabName: 'Buckets',
-      }
-      this.router.navigate(['/buckets'], { queryParams: { name: 'Archives' } })
-    })
+      };
+      this.router.navigate(['/buckets'], { queryParams: { name: 'Archives' } });
+    });
   }
 
   getBucketsData(bucketName: any) {
     return new Promise((resolve, reject) => {
-      this.bucketNameGlobal = bucketName
-      this.singleBucketsData = []
+      this.bucketNameGlobal = bucketName;
+      this.singleBucketsData = [];
       this.http.get(apiurls.bucketsData + bucketName).subscribe((res: any) => {
         console.log(res);
         this.singleBucketsData.push(...res);
-        resolve(true)
-      })
-    })
+        resolve(true);
+      });
+    });
   }
 
   getBucketsDataByPath(path: any, folderName: any) {
-    this.loopArray(this.singleBucketsData, path)
+    this.loopArray(this.singleBucketsData, path);
   }
 
   loopArray(arr: any, path: any) {
     for (let i = 0; i < arr.length; i++) {
-      if (arr[i].type == "folder") {
+      if (arr[i].type == 'folder') {
         if (arr[i].path == path) {
-          this.bucketsView = false
+          this.bucketsView = false;
           let data = {
             files: arr[i].files,
             folderName: arr[i].folderName,
-            type: "folder",
-            path: path
-          }
-          this.singleBucketsData = [data]
-          return
+            type: 'folder',
+            path: path,
+          };
+          this.singleBucketsData = [data];
+          return;
         } else {
           this.loopArray(arr[i].files, path);
         }
       }
-
     }
   }
 
-
   bucketClick(bucketName: any) {
-
-    this.BackIndexVal = this.BackIndexVal + 1
-    this.bucketsView = false
-    this.singleBucketsData = []
+    this.BackIndexVal = this.BackIndexVal + 1;
+    this.bucketsView = false;
+    this.singleBucketsData = [];
     let data = {
       folder: bucketName,
-      path: bucketName
-    }
+      path: bucketName,
+    };
 
     if (!this.dataToSend.includes(data)) {
-      this.dataToSend.push(data)
+      this.dataToSend.push(data);
     }
     const queryParams = {
       mainbucket: bucketName,
@@ -141,39 +140,45 @@ export class BucketsComponent implements OnInit {
       backIndex: this.BackIndexVal,
       frontIndex: this.FrontIndexVal,
       data: JSON.stringify(this.dataToSend),
-    }
+    };
     // this.TabNa
     this.router.navigate(['/buckets'], { queryParams: queryParams });
-
   }
 
-  innerFolderNavigation(InnerFiles: any, bucketName: any, folderName: any, path: any, files: any) {
+  innerFolderNavigation(
+    InnerFiles: any,
+    bucketName: any,
+    folderName: any,
+    path: any,
+    files: any
+  ) {
     for (let file in files.files) {
-      if (files.files[file].type == "file") {
-        this.FileTypes.push(files.files[file].filetype)
+      if (files.files[file].type == 'file') {
+        this.FileTypes.push(files.files[file].filetype);
       }
     }
-    this.BackIndexVal = this.BackIndexVal + 1
+    this.BackIndexVal = this.BackIndexVal + 1;
 
     let innerData = {
       folder: folderName,
-      path: path
-    }
+      path: path,
+    };
 
-    let isFolderExist = this.dataToSend.some((res: any) => res.folder == innerData.folder)
+    let isFolderExist = this.dataToSend.some(
+      (res: any) => res.folder == innerData.folder
+    );
     if (!isFolderExist) {
-      this.dataToSend.push(innerData)
+      this.dataToSend.push(innerData);
     }
-
 
     let data = {
       files: InnerFiles,
       folderName: folderName,
-      type: "folder",
-      path: path
-    }
-    this.singleBucketsData = [data]
-    console.table(this.singleBucketsData)
+      type: 'folder',
+      path: path,
+    };
+    this.singleBucketsData = [data];
+    console.table(this.singleBucketsData);
 
     this.queryParams = {
       path: path,
@@ -183,13 +188,15 @@ export class BucketsComponent implements OnInit {
       data: JSON.stringify(this.dataToSend),
     };
     this.router.navigate(['/buckets'], { queryParams: this.queryParams });
-
   }
 
   clickFolder(value: any) {
     for (let i in this.dataToSend) {
       if (value?.folder == this.dataToSend[i]?.folder) {
-        this.dataToSend = this.dataToSend.slice(0, this.dataToSend?.indexOf(this.dataToSend[i]) + 1)
+        this.dataToSend = this.dataToSend.slice(
+          0,
+          this.dataToSend?.indexOf(this.dataToSend[i]) + 1
+        );
       }
     }
 
@@ -201,168 +208,164 @@ export class BucketsComponent implements OnInit {
       data: JSON.stringify(this.dataToSend),
     };
     this.router.navigate(['/buckets'], { queryParams: queryParams });
-
   }
-
 
   type(type: any) {
-    this.viewtype = type || localStorage.getItem('view')
+    this.viewtype = type || localStorage.getItem('view');
     this.route.queryParamMap.subscribe((params: any) => {
       this.router.navigate(['/buckets'], { queryParams: params.params });
-    })
+    });
   }
-
-
 
   lastmodified(type: string) {
-    let modified: any = []
+    let modified: any = [];
     if (type == 'asc') {
-      this.createdArrow = false
+      this.createdArrow = false;
       this.allBuckets.filter((buckets: any) => {
         if (buckets) {
-          buckets.created = new Date(buckets.created)
-          modified.push(buckets)
+          buckets.created = new Date(buckets.created);
+          modified.push(buckets);
         }
-      })
-      this.allBuckets = modified.sort((a: any, b: any) => a.created.getTime() - b.created.getTime());
+      });
+      this.allBuckets = modified.sort(
+        (a: any, b: any) => a.created.getTime() - b.created.getTime()
+      );
     }
-
 
     if (type == 'dec') {
-
-      this.createdArrow = true
+      this.createdArrow = true;
       this.allBuckets.filter((buckets: any) => {
-        buckets.created = new Date(buckets.created)
-        modified.push(buckets)
-      })
-      this.allBuckets = modified.sort((a: any, b: any) => b.created.getTime() - a.created.getTime());
+        buckets.created = new Date(buckets.created);
+        modified.push(buckets);
+      });
+      this.allBuckets = modified.sort(
+        (a: any, b: any) => b.created.getTime() - a.created.getTime()
+      );
     }
-    if (type == "size") {
-      let folderArr: any = []
+    if (type == 'size') {
+      let folderArr: any = [];
       this.singleBucketsData.filter((res: any) => {
-        modified = []
+        modified = [];
         res?.files?.filter((file: any) => {
-          if (file?.type == "file") {
+          if (file?.type == 'file') {
             modified.push(file);
           } else {
-            folderArr.push(file)
+            folderArr.push(file);
           }
-        })
+        });
 
-        modified = modified?.sort((a: any, b: any) => a.size_value - b.size_value)
-        this.singleBucketsData = [...modified, ...folderArr]
-      })
+        modified = modified?.sort(
+          (a: any, b: any) => a.size_value - b.size_value
+        );
+        this.singleBucketsData = [...modified, ...folderArr];
+      });
     }
 
-    if (type == "size-asc") {
-      this.sizeArrow = false
+    if (type == 'size-asc') {
+      this.sizeArrow = false;
       this.allBuckets.filter((buckets: any) => {
         if (buckets) {
-          modified.push(buckets)
+          modified.push(buckets);
         }
-      })
-      this.allBuckets = modified.sort((a: any, b: any) => a.size_value - b.size_value);
-
+      });
+      this.allBuckets = modified.sort(
+        (a: any, b: any) => a.size_value - b.size_value
+      );
     }
-    if (type == "size-dec") {
-      this.sizeArrow = true
+    if (type == 'size-dec') {
+      this.sizeArrow = true;
       this.allBuckets.filter((buckets: any) => {
         if (buckets) {
-          modified.push(buckets)
+          modified.push(buckets);
         }
-      })
-      this.allBuckets = modified.sort((a: any, b: any) => b.size_value - a.size_value);
+      });
+      this.allBuckets = modified.sort(
+        (a: any, b: any) => b.size_value - a.size_value
+      );
     }
 
-    if (type == "name-asc") {
-      this.nameArrow = false
+    if (type == 'name-asc') {
+      this.nameArrow = false;
       this.allBuckets.filter((buckets: any) => {
         if (buckets) {
-          modified.push(buckets)
+          modified.push(buckets);
         }
-      })
+      });
 
-      this.allBuckets = modified.slice().sort((a: any, b: any) => a.name.localeCompare(b.name));
+      this.allBuckets = modified
+        .slice()
+        .sort((a: any, b: any) => a.name.localeCompare(b.name));
     }
-    if (type == "name-dec") {
-      this.nameArrow = true
+    if (type == 'name-dec') {
+      this.nameArrow = true;
       this.allBuckets.filter((buckets: any) => {
         if (buckets) {
-          modified.push(buckets)
+          modified.push(buckets);
         }
-      })
+      });
 
-      this.allBuckets = modified.slice().sort((a: any, b: any) => b.name.localeCompare(a.name));
+      this.allBuckets = modified
+        .slice()
+        .sort((a: any, b: any) => b.name.localeCompare(a.name));
     }
-
   }
 
-
-  //Download File 
-
+  //Download File
 
   downloadFile(bucketName: any, file: any) {
-    let data: any = {
-
-    }
-    let name
+    let data: any = {};
+    let name;
     for (let i in bucketName) {
-      if (i == "path") {
-        name = bucketName[i].split("/")[0]
-        data.bucket_name = name
-        data.path = file.fileurl
+      if (i == 'path') {
+        name = bucketName[i].split('/')[0];
+        data.bucket_name = name;
+        data.path = file.fileurl;
       }
     }
     this.http.post(apiurls.downloadFile, data).subscribe((res: any) => {
-      window.open(res.download_url)
-    })
-
+      // window.open(res.download_url);
+      console.log(res.download_url.split());
+    });
   }
-
 
   //Download Folder
 
   downloadFolder(folderName: any, BucketName: any) {
     console.log(folderName.path.split('/').slice(1).join('/'));
 
-    let folderData: any = {
-
-    }
-    folderData.bucket_name = folderName.path.split('/')[0]
-    folderData.folder_path = folderName.path.split('/').slice(1).join('/')
+    let folderData: any = {};
+    folderData.bucket_name = folderName.path.split('/')[0];
+    folderData.folder_path = folderName.path.split('/').slice(1).join('/');
     this.http.post(apiurls.folderDownload, folderData).subscribe((res: any) => {
-      // console.log(res, "url");
-      window.open(res.download_url)
-    })
+      window.open(res.download_url);
+    });
   }
   newBukcet(val: any) {
     if (val) {
-      this.getBuckets()
+      this.getBuckets();
     }
   }
 
   openFile(bucketName: any, file: any) {
-    let data: any = {
-
-    }
-    let name
+    let data: any = {};
+    let name;
     for (let i in bucketName) {
-      if (i == "path") {
-        name = bucketName[i].split("/")[0]
-        data.bucket_name = name
-        data.path = file.fileurl
+      if (i == 'path') {
+        name = bucketName[i].split('/')[0];
+        data.bucket_name = name;
+        data.path = file.fileurl;
       }
     }
     this.http.post(apiurls.downloadFile, data).subscribe((res: any) => {
-      window.open(res.download_url, '_blank')
-    })
+      window.open(res.download_url, '_blank');
+    });
   }
 
   getBucketsBySearch(val: any) {
-    this.allBuckets = val
+    this.allBuckets = val;
   }
   getInnerBucketDataBySearch(innerData: any) {
     console.log(innerData);
-    this.singleBucketsData = innerData
+    this.singleBucketsData = innerData;
   }
 }
--- src/app/components/user-management/user-management.component.ts
+++ src/app/components/user-management/user-management.component.ts
@@ -9,81 +9,87 @@ import { apiurls } from 'src/app/shared/apiurls';
 @Component({
   selector: 'app-user-management',
   templateUrl: './user-management.component.html',
-  styleUrls: ['./user-management.component.scss']
+  styleUrls: ['./user-management.component.scss'],
 })
 export class UserManagementComponent implements OnInit {
-  TabName: any
+  TabName: any;
+  // dropdownSettings = {};
   dropdownSettings = {};
-  selectMsg: any = false
-  spinnerBtn: any = true
-  spinner = false
-  btn_close = false
-  constructor(private router: ActivatedRoute, private route: Router, private http: HttpClient, private toastr: ToastrService, private modalservice: NgbModal) {
-
+  selectMsg: any = false;
+  spinnerBtn: any = true;
+  spinner = false;
+  btn_close = false;
+  constructor(
+    private router: ActivatedRoute,
+    private route: Router,
+    private http: HttpClient,
+    private toastr: ToastrService,
+    private modalservice: NgbModal
+  ) {
     this.dropdownSettings = {
       singleSelection: false,
-      itemsShowLimit: 4
+      itemsShowLimit: 4,
     };
   }
-  allUsers: any = []
+  allUsers: any = [];
 
-  addbucket: any = {}
-  allBuckets: any = []
-  userData: any = {}
+  addbucket: any = {};
+  allBuckets: any = [];
+  userData: any = {};
   ngOnInit(): void {
-    localStorage.setItem('tabname', 'User management')
+    localStorage.setItem('tabname', 'User management');
 
-    this.getBuckets()
-    this.getAllUsers()
+    this.getBuckets();
+    this.getAllUsers();
     let headername = {
-      name: "User management"
-    }
-    this.route.navigate(['/user-management'], { queryParams: headername })
+      name: 'User management',
+    };
+    this.route.navigate(['/user-management'], { queryParams: headername });
     this.router.queryParamMap.subscribe((res: any) => {
-      this.TabName = res.params.name
-    })
+      this.TabName = res.params.name;
+    });
   }
   getAllUsers() {
     this.http.get(apiurls.allUsers).subscribe((res: any) => {
-      this.allUsers = res
-    })
+      this.allUsers = res;
+    });
   }
 
-
   enableUser(username: any, status: any) {
-    let UserStatus = status
+    let UserStatus = status;
     let data: any = {
       username: username,
-    }
+    };
     if (UserStatus == 'enabled') {
-      data.action = false
-    }
-    else if (UserStatus == 'disabled') {
-      data.action = true
+      data.action = false;
+    } else if (UserStatus == 'disabled') {
+      data.action = true;
     }
     this.http.post(apiurls.enableuser, data).subscribe((res: any) => {
       console.log(res.message);
-      if (res.message == "User disable is successful") {
-        this.toastr.error(`Username disable is successful`, '', { timeOut: 1000 })
+      if (res.message == 'User disable is successful') {
+        this.toastr.success(`Username disable is successful`, '', {
+          timeOut: 1000,
+        });
       }
-      if (res.message == "User enable is successful") {
-        this.toastr.success(`Username enable is successful`, '', { timeOut: 1000 })
+      if (res.message == 'User enable is successful') {
+        this.toastr.success(`Username enable is successful`, '', {
+          timeOut: 1000,
+        });
       }
-      this.getAllUsers()
-    })
+      this.getAllUsers();
+    });
   }
 
-
-
   viewUserBuckets(user: any) {
-    this.userData.username = user.name
-    this.userData.buckets = user?.bucketname?.split(',')
+    this.userData.username = user.name;
+    this.userData.buckets = user?.bucketname?.split(',');
   }
   //open modal
   open(content: any, user: any) {
-    this.modalservice.open(content, { backdrop: 'static' })
-    this.addbucket.username = user?.name
-    this.addbucket.buckets = user?.bucketname?.split(',')
+    this.modalservice.open(content, { backdrop: 'static' });
+    this.addbucket.username = user?.name;
+    this.addbucket.buckets = user?.bucketname?.split(',');
     // let userbuckets: any = user.bucketname.split(',')
     // let arr: any = []
     // let val = this.allBuckets.filter((item: any) => !userbuckets.includes(item)).concat(userbuckets.filter((item: any) => !this.allBuckets.includes(item)))
@@ -92,53 +98,47 @@ export class UserManagementComponent implements OnInit {
 
   //add buckets to user
   addUserBucket(form: NgForm, user: any) {
-    this.spinnerBtn = false
-    this.spinner = true
+    this.spinnerBtn = false;
+    this.spinner = true;
     if (!this.addbucket.buckets) {
-
-      this.spinnerBtn = true
-      this.spinner = false
-      this.selectMsg = true
+      this.spinnerBtn = true;
+      this.spinner = false;
+      this.selectMsg = true;
       setTimeout(() => {
-        this.selectMsg = false
-      }, 1500)
-    }
-    else {
+        this.selectMsg = false;
+      }, 1500);
+    } else {
       console.log(form.value);
-      let selectedBuckets: any
-      selectedBuckets = form?.value?.buckets?.join(',')
-      form.value.buckets = selectedBuckets
+      let selectedBuckets: any;
+      selectedBuckets = form?.value?.buckets?.join(',');
+      form.value.buckets = selectedBuckets;
       this.http.post(apiurls.addBuckets, form.value).subscribe((res: any) => {
         console.log(form.value);
-        this.spinnerBtn = true
-        this.spinner = false
+        this.spinnerBtn = true;
+        this.spinner = false;
         this.modalservice.dismissAll();
-        this.getAllUsers()
-        this.toastr.success('Buckets updated successfully')
-      })
+        this.getAllUsers();
+        this.toastr.success('Buckets updated successfully');
+      });
     }
-
   }
-
 
   //get buckets
   getBuckets() {
-    this.allBuckets = []
+    this.allBuckets = [];
     this.http.get(apiurls.buckets).subscribe((res: any) => {
-
       for (let i in res.buckets) {
-        this.allBuckets.push(res.buckets[i].name)
+        this.allBuckets.push(res.buckets[i].name);
       }
-    })
+    });
   }
   userCreated(val: any) {
     if (val) {
-      this.getAllUsers()
+      this.getAllUsers();
     }
   }
   searchUsers(users: any) {
     console.log(users);
-    this.allUsers = users
+    this.allUsers = users;
   }
 }
-
